429b2b103035a697a06b8e0063477f7f7e024aff
janosadam11 <testelemer@localhost.localdomain>
2024-03-04 16:03:46 +0100

    test

--- git.sh
+++ git.sh
@@ -1,16 +1,46 @@
 #!/bin/bash
-# Script generating commits
-for (( i=$2; i >= 1; i-- ))
-do
 
-if [ $(($i % 2)) -eq 0 ]
-    then
-    echo "$1" >> $1
-    else
-    sed -i "$ d" test
-    fi
+# Make an array of repository directories,which you want to auto-commit
+repo_dirs=(
+    "/home/testelemer/Commitgenerator"
+    # Add more repository directories,if needed
+    
+)
 
+# Function to navigate to a repository directory, add changes,
+# and commit every few hours
+perform_git_operations() {
+    local repo_dir=$1
+
+    # Navigate to the repo directory
+    cd "$repo_dir" || return
+
+    # Add all changes to the staging area
     git add .
-    date_commit=$(date +"%s" -d "$i day ago")
-    GIT_COMMITTER_DATE="$date_commit" git commit -m "Change $1" --date "$date_commit"
+
+    # Commit the changes with a timestamp
+    while true; do
+        git commit -m "Automated commit on $(date)" # change msg as needed
+        sleep 1s # change 6 with any value
+    done
+}
+
+# Loop over the repo directories and run the same script for each one
+for repo_dir in "${repo_dirs[@]}"; do
+    # Run the script initially for each repo directory
+    perform_git_operations "$repo_dir" &
+done
+
+# Loop indefinitely and push any changes, every few minutes
+while true; do
+    sleep 10m # change 15 with any value
+
+    # Loop through the repo directories and push changes for each one
+    for repo_dir in "${repo_dirs[@]}"; do
+        # Navigate to the repository directory
+        cd "$repo_dir" || continue
+
+        # Push the changes to the remote repo's 'main' branch
+        git push origin main
+    done
 done
